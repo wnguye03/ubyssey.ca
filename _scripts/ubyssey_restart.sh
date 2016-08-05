d34f30e8f9513a44eb7f2c34d2238fa216ab6d75
Peter Siemens <peterjsiemens@gmail.com>
2016-08-05 11:23:32 -0700

    Set owner before executing

--- _scripts/ubyssey_restart.sh
+++ _scripts/ubyssey_restart.sh
@@ -6,5 +6,6 @@ if [ -f run/ubyssey.pid ]; then
 fi
 
 # Start the server
-sudo chmod g+x ${BASH_SOURCE%/*}/gunicorn_start
+sudo chown ubyssey ${BASH_SOURCE%/*}/gunicorn_start
+sudo chmod u+x ${BASH_SOURCE%/*}/gunicorn_start
 sudo -u ubyssey -H sh -c "_scripts/gunicorn_start"
--- _scripts/ubyssey_start.sh
+++ _scripts/ubyssey_start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Start the server
+sudo chown ubyssey ${BASH_SOURCE%/*}/gunicorn_start
 sudo chmod g+x ${BASH_SOURCE%/*}/gunicorn_start
 sudo -u ubyssey -H sh -c "${BASH_SOURCE%/*}/gunicorn_start"
