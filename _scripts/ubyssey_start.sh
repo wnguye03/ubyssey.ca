37f973a7bb65f60b9b36928110ca78e2a4f509c1
Peter Siemens <peterjsiemens@gmail.com>
2016-08-05 11:19:05 -0700

    Use group executable permissions

--- _scripts/ubyssey_start.sh
+++ _scripts/ubyssey_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Start the server
-sudo chmod u+x ${BASH_SOURCE%/*}/gunicorn_start
+sudo chmod g+x ${BASH_SOURCE%/*}/gunicorn_start
 sudo -u ubyssey -H sh -c "${BASH_SOURCE%/*}/gunicorn_start"
