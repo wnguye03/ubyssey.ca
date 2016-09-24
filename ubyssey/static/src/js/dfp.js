4efd42f4a21349e0264b6bdb3bd56785d63c43a3
Peter Siemens <peterjsiemens@gmail.com>
2016-09-23 18:43:03 -0700

    Refactor DFP script into class

--- ubyssey/static/src/js/dfp.js
+++ ubyssey/static/src/js/dfp.js
@@ -4,9 +4,32 @@ var sizes = {
     'mobile-leaderboard': [300, 50]
 };
 
-var adslots = [];
+var googletag = googletag || {};
+googletag.cmd = googletag.cmd || [];
 
-function initAds(){
+window.googletag = googletag;
+
+function DFP(element) {
+
+  var self = this;
+
+  // Set default values
+  self.adslots = [];
+  self.element = document;
+
+  // Inject GPT script tag into document
+  (function() {
+    var gads = document.getElementById('gpt-script') || document.createElement('script');
+    gads.id = 'gpt-script';
+    gads.async = true;
+    gads.type = 'text/javascript';
+    var useSSL = 'https:' == document.location.protocol;
+    gads.src = (useSSL ? 'https:' : 'http:') + '//www.googletagservices.com/tag/js/gpt.js';
+    var node = document.getElementsByTagName('script')[0];
+    node.parentNode.insertBefore(gads, node);
+  })();
+
+  function setup() {
     // Infinite scroll requires SRA
     googletag.pubads().enableSingleRequest();
 
@@ -17,29 +40,52 @@ function initAds(){
 
     // Enable services
     googletag.enableServices();
-};
+  }
 
-function collectAds(element){
-    var dfpslots = $(element).find(".adslot").filter(":visible");
+  function collectAds() {
+    var dfpslots = $(self.element).find(".adslot").filter(":visible");
+
+    googletag.destroySlots();
+
     $(dfpslots).each(function(){
-        var slotName = $(this).attr('id');
-        adslots.push([
-            slotName,
-            googletag.defineSlot('/61222807/'+$(this).data('dfp'), sizes[$(this).data('size')], slotName)
-                .addService(googletag.pubads())
-            ]);
+      var slotName = $(this).attr('id'),
+          slot = googletag.defineSlot('/61222807/'+$(this).data('dfp'), sizes[$(this).data('size')], slotName);
+
+      self.adslots.push([
+        slotName,
+        slot.addService(googletag.pubads())
+      ]);
     });
+  }
+
+  function refreshAds() {
+    $.each(self.adslots, function(i, slot) {
+      googletag.display(slot[0]);
+      googletag.pubads().refresh([slot[1]]);
+    });
+  };
+
+  return {
+    load: function(element) {
+      self.element = element;
+      googletag.cmd.push(setup);
+      googletag.cmd.push(collectAds);
+      googletag.cmd.push(refreshAds);
+    },
+    reset: function() {
+      self.adslots = [];
+      googletag.cmd.push(googletag.destroySlots);
+    }
+  }
 }
 
-function refreshAds(){
-    $.each(adslots, function(i, slot){
-        googletag.display(slot[0]);
-        googletag.pubads().refresh([slot[1]]);
-    });
-};
+var dfp = DFP();
 
-$(document).ready(function(){
-    googletag.cmd.push(function() { initAds() });
-    googletag.cmd.push(function() { collectAds(document); });
-    googletag.cmd.push(function() { refreshAds() });
+$(document).ready(function() {
+  dfp.load(document);
 });
+
+window.resetAds = function(element) {
+  dfp.reset();
+  dfp.load(element);
+}
