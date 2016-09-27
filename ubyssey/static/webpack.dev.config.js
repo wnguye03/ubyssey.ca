2756b8c4915dfa626908189991d008054385fd6e
Peter Siemens <peterjsiemens@gmail.com>
2016-09-27 10:21:44 -0700

    Fix dev config to not use Object.assign

--- ubyssey/static/webpack.dev.config.js
+++ ubyssey/static/webpack.dev.config.js
@@ -1,14 +1,16 @@
 var webpack = require('webpack');
 var prodConfig = require('./webpack.config.js');
 
-module.exports = Object.assign({}, prodConfig, {
-  devtool: 'source-map',
-  debug: true,
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('development')
-      }
-    })
-  ]
-});
+var devConfig = Object.create(prodConfig);
+
+devConfig.devtool = 'source-map';
+devConfig.debug = true;
+devConfig.plugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('development')
+    }
+  })
+];
+
+module.exports = devConfig;
