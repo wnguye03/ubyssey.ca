f801e45c57075e8bfaf79bbb00e61acd4f4a9faf
Matthew Chernoff <chernoff.matthew@gmail.com>
2017-01-03 22:07:24 -0800

    Separated feeds by section, with additional front page feed

--- ubyssey/feed.py
+++ ubyssey/feed.py
@@ -1,26 +1,71 @@
 from django.contrib.syndication.views import Feed
 from django.core.urlresolvers import reverse
 
-from dispatch.apps.content.models import Article
+from dispatch.apps.core.models import Person
+from dispatch.apps.content.models import Section, Article, Author
 
 from ubyssey.helpers import ArticleHelper
 
-class FrontpageFeed(Feed):
-    title = 'Ubyssey Front Page'
-    link = '/'
-    description = 'Updates from the Ubyssey'
+class SectionFeed(Feed):
 
     def __init__(self, max_items=10):
         self.max_items = max_items
+
+    def get_object(self, request, slug):
+        return Section.objects.get(name=slug)
+
+    def title(self, section):
+        return 'Ubyssey %s' % section.name
+
+    def description(self, section):
+        return 'Daily updates from Ubyssey %s' % section.name
+
+    def link(self, section):
+        return '/%s' % section.name
         
-    def items(self):
-        return ArticleHelper.get_frontpage()[:self.max_items]
+    def items(self, section):
+        return Article.objects.filter(section=section, is_published=True).order_by('-published_at')[:self.max_items]
+       
+    def item_title(self, item):
+        return item.headline
+
+    def item_pubdate(self, item):
+        return item.published_at
+
+    def item_description(self, item):
+        return item.snippet
+
+    def item_author_name(self, item):
+        auth = Author.objects.get(article_id=item.id)
+        return Person.objects.get(pk=auth.person_id).full_name
+
+    def item_link(self, item):
+        return reverse('article', args=[item.section_id,item.slug])
+    
+class FrontpageFeed(Feed):
+
+    title = 'Ubyssey Front Page'
+    link = '/'
+    description = 'Daily updates from The Ubyssey'
+
+    def __init__(self, max_items=10):
+        self.max_items = max_items
+
+    def items(self, section):
+        return ArticleHelper.get_frontpage(limit = self.max_items)
 
     def item_title(self, item):
         return item.headline
 
+    def item_pubdate(self, item):
+        return item.published_at
+
     def item_description(self, item):
         return item.snippet
+
+    def item_author_name(self, item):
+        auth = Author.objects.get(article_id=item.id)
+        return Person.objects.get(pk=auth.person_id).full_name
 
     def item_link(self, item):
         return reverse('article', args=[item.section_id,item.slug])
