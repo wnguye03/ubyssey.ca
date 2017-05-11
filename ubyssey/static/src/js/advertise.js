39d70071e0b3da37ee1472b37e63fd98c5d05adb
Peter Siemens <peterjsiemens@gmail.com>
2017-05-11 16:26:09 -0700

    More changes

--- ubyssey/static/src/js/advertise.js
+++ ubyssey/static/src/js/advertise.js
@@ -1,7 +1,10 @@
 $(function() {
-  $('.o-placements__placement').click(function() {
-    $('.o-placements__placement').removeClass('o-placements__placement--active');
+
+  $('.o-placements--web .o-placements__placement').click(function() {
+    console.log('click');
+    $('.o-placements--web .o-placements__placement').removeClass('o-placements__placement--active');
     $(this).addClass('o-placements__placement--active');
-    $('.o-placements__demo').attr('data-demo', $(this).data('demo'));
+    $('.o-placements--web .o-placements__demo__inner img').css('top', $(this).data('offset'));
   });
+
 });
