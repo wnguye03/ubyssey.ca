51ee4e01b31d2cc1dcc1351b91dcf610cc6a449b
Peter Siemens <peterjsiemens@gmail.com>
2017-05-12 11:59:25 -0700

    Print ad placement demo

--- ubyssey/static/src/js/advertise.js
+++ ubyssey/static/src/js/advertise.js
@@ -1,10 +1,16 @@
 $(function() {
 
   $('.o-placements--web .o-placements__placement').click(function() {
-    console.log('click');
     $('.o-placements--web .o-placements__placement').removeClass('o-placements__placement--active');
     $(this).addClass('o-placements__placement--active');
     $('.o-placements--web .o-placements__demo__inner img').css('top', $(this).data('offset'));
+  });
+
+  $('.o-placements--print .o-placements__placement').click(function() {
+    $('.o-placements--print .o-placements__placement').removeClass('o-placements__placement--active');
+    $(this).addClass('o-placements__placement--active');
+
+    $('.o-placements--print .o-placements__demo').attr('data-demo', $(this).data('demo'));
   });
 
 });
