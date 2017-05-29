a9ed83b22ae2e696b8d27b10b0411f050720ae57
Peter Siemens <peterjsiemens@gmail.com>
2017-05-29 10:07:56 -0700

    Production schedule, smooth scrolling

--- ubyssey/static/src/js/advertise.js
+++ ubyssey/static/src/js/advertise.js
@@ -1,5 +1,12 @@
 $(function() {
 
+  // Navigation links smooth scrolling
+  $('a[href*=\\#]').on('click', function(event){
+    console.log(event);
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+  });
+
   $('.o-placements--web .o-placements__placement').click(function() {
     $('.o-placements--web .o-placements__placement').removeClass('o-placements__placement--active');
     $(this).addClass('o-placements__placement--active');
@@ -11,6 +18,24 @@ $(function() {
     $(this).addClass('o-placements__placement--active');
 
     $('.o-placements--print .o-placements__demo').attr('data-demo', $(this).data('demo'));
+  });
+
+  // Open modal
+  $('.c-production-schedule__view').click(function() {
+    $('.c-production-schedule__modal').show();
+  })
+
+  // Close modal
+  $('.c-production-schedule__modal__close').click(function() {
+    $('.c-production-schedule__modal').hide();
+  });
+
+  $('.c-production-schedule__modal').click(function() {
+    $('.c-production-schedule__modal').hide();
+  });
+
+  $('.c-production-schedule__modal img').click(function(e) {
+    e.stopPropagation()
   });
 
 });
