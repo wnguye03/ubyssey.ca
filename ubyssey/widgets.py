643d4f86c36088e2c179dfc5a2ee7cc0b533fe32
Axel-Jacobsen <axelnj44@gmail.com>
2017-07-12 18:55:11 -0700

    event sidebar first draft

--- ubyssey/widgets.py
+++ ubyssey/widgets.py
@@ -1,9 +1,11 @@
-<<<<<<< HEAD
+from datetime import datetime
+
 from dispatch.theme.fields import CharField, TextField, ArticleField, ImageField, EventField, InvalidField
 from dispatch.theme import register
 from dispatch.theme.widgets import Zone, Widget
 from dispatch.apps.events.models import Event
 from zones import EventSidebar
+
 
 @register.widget
 class SingleEventWidget(Widget):
@@ -11,27 +13,27 @@ class SingleEventWidget(Widget):
     id = 'single-event'
     name = 'Single Event'
 
-    template = 'templates/single_event_widget.html'
+    template = 'widgets/single_event_widget.html'
 
-    zones = [Sidebar]
+    zones = [EventSidebar]
 
-    title = CharField('Featured Event')
+    title = CharField('Event Title')
 
     def context(self, data):
 
-        event = Event.objcts.filter(is_published=True).order_by('start_time')[:1]
+        event = Event.objects.filter(is_published=True).order_by('start_time')[:1][0]
 
-        print event.start_time
-
-        data['events'].append({
+        data['events'] = {
             'title': event.title,
             'start_day': event.start_time.day,
-            'start_month': event.start_time.month,
+            'start_month': event.start_time.strftime('%b'),
             'start_time': event.start_time,
             'end_time': event.end_time,
-            'image': event.image,
-            'location': event.loction
-        })
+            'image': event.image.url,
+            'location': event.location
+        }
+
+        return data
 
 @register.widget
 class EventWidget(Widget):
