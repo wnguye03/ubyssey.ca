0fab7168a7f9e4d89e48eb51f98fd367962d8bd4
Jonathan Chapple <joncpl@gmail.com>
2017-07-13 18:54:26 -0700

    implement upcoming events widget without styles

--- ubyssey/widgets.py
+++ ubyssey/widgets.py
@@ -1,7 +1,11 @@
+from datetime import date
+
 from dispatch.theme import register
 from dispatch.theme.widgets import Widget
 from dispatch.theme.fields import CharField, EventField
-from zones import EventSidebar
+from dispatch.apps.events.models import Event
+
+from zones import EventSidebar, HomePageSidebar
 
 @register.widget
 class EventWidget(Widget):
@@ -10,6 +14,38 @@ class EventWidget(Widget):
   template = 'widgets/event.html'
   zones = (EventSidebar,)
 
-  title = CharField('title')
+  title = CharField('Title')
 
-  event = EventField('event')
+  event = EventField('Event')
+
+@register.widget
+class UpcomingEvents(Widget):
+  id = 'upcoming-events'
+  name = 'Upcoming Events'
+  template = 'widgets/upcoming-events.html'
+  zones = (HomePageSidebar,)
+
+  featured_events = EventField('Featured Events', many=True)
+  number_of_events = CharField('Number of Events')
+
+  def prepare_data(self):
+    result = super(UpcomingEvents, self).prepare_data()
+    today = date.today()
+
+    N = int(result['number_of_events'], base=10)
+
+    featured_ids = []
+    if isinstance(result['featured_events'], list):
+        for e in result['featured_events']:
+            featured_ids.append(e.pk)
+
+    events = Event.objects \
+        .filter(is_submission=False) \
+        .filter(is_published=True) \
+        .filter(start_time__gt=today) \
+        .exclude(pk__in=featured_ids) \
+        .order_by('start_time')[:N]
+
+    result['upcoming'] = events
+
+    return result
--- ubyssey/zones.py
+++ ubyssey/zones.py
@@ -3,5 +3,10 @@ from dispatch.theme.widgets import Zone
 
 @register.zone
 class EventSidebar(Zone):
-  id = 'event-sidebar'
-  name = 'Event Sidebar'
+    id = 'event-sidebar'
+    name = 'Event Sidebar'
+
+@register.zone
+class HomePageSidebar(Zone):
+    id = 'homepage-sidebar'
+    name = 'Homepage Sidebar'
