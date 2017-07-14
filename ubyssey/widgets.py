a1409a0e6f6f6a6699d85e84e9f1eece02236c07
Jonathan Chapple <joncpl@gmail.com>
2017-07-13 21:21:31 -0700

    Style upcoming widgets
    Also differentiate between bottom and top sidebar zones on homepage

--- ubyssey/widgets.py
+++ ubyssey/widgets.py
@@ -5,45 +5,39 @@ from dispatch.theme.widgets import Widget
 from dispatch.theme.fields import CharField, EventField
 from dispatch.apps.events.models import Event
 
-from zones import EventSidebar, HomePageSidebar
-
-@register.widget
-class EventWidget(Widget):
-  id = 'custom'
-  name = 'Event Widget'
-  template = 'widgets/event.html'
-  zones = (EventSidebar,)
-
-  title = CharField('Title')
-
-  event = EventField('Event')
+from zones import HomePageSidebar, HomePageSidebarBottom
 
 @register.widget
 class UpcomingEvents(Widget):
   id = 'upcoming-events'
   name = 'Upcoming Events'
   template = 'widgets/upcoming-events.html'
-  zones = (HomePageSidebar,)
+  zones = (HomePageSidebar, HomePageSidebarBottom)
 
-  featured_events = EventField('Featured Events', many=True)
+  featured_event = EventField('Featured Events', many=False)
   number_of_events = CharField('Number of Events')
 
   def prepare_data(self):
+    """Overide prepare_data to add the next N events occuring to the context"""
+
     result = super(UpcomingEvents, self).prepare_data()
-    today = date.today()
 
-    N = int(result['number_of_events'], base=10)
+    try:
+        N = int(result['number_of_events'], base=10)
+    except:
+        N = 5
 
-    featured_ids = []
-    if isinstance(result['featured_events'], list):
-        for e in result['featured_events']:
-            featured_ids.append(e.pk)
+    # exclude the featured event from showing up in the other list
+    if result['featured_event']:
+        featured_id = result['featured_event'].pk
+    else:
+        featured_id = None
 
     events = Event.objects \
         .filter(is_submission=False) \
         .filter(is_published=True) \
-        .filter(start_time__gt=today) \
-        .exclude(pk__in=featured_ids) \
+        .filter(start_time__gt=date.today()) \
+        .exclude(pk=featured_id) \
         .order_by('start_time')[:N]
 
     result['upcoming'] = events
--- ubyssey/zones.py
+++ ubyssey/zones.py
@@ -2,11 +2,11 @@ from dispatch.theme import register
 from dispatch.theme.widgets import Zone
 
 @register.zone
-class EventSidebar(Zone):
-    id = 'event-sidebar'
-    name = 'Event Sidebar'
-
-@register.zone
 class HomePageSidebar(Zone):
     id = 'homepage-sidebar'
     name = 'Homepage Sidebar'
+
+@register.zone
+class HomePageSidebarBottom(Zone):
+    id = 'homepage-sidebar-bottom'
+    name = 'Homepage Sidebar Bottom'
