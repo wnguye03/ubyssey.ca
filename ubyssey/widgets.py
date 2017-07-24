9f2780789f2ea7fb995a10c4db0ae725e575f326
Peter Siemens <peterjsiemens@gmail.com>
2017-07-24 12:25:05 -0400

    Restructure widgets

--- ubyssey/widgets.py
+++ ubyssey/widgets.py
@@ -4,36 +4,8 @@ from dispatch.theme.fields import CharField, TextField, ArticleField, ImageField
 from dispatch.theme import register
 from dispatch.theme.widgets import Zone, Widget
 from dispatch.apps.events.models import Event
-from zones import EventSidebar
 
-
-@register.widget
-class SingleEventWidget(Widget):
-
-    id = 'single-event'
-    name = 'Single Event'
-
-    template = 'widgets/single_event_widget.html'
-
-    zones = [EventSidebar]
-
-    title = CharField('Event Title')
-
-    def context(self, data):
-
-        event = Event.objects.filter(is_published=True).order_by('start_time')[:1][0]
-
-        data['events'] = {
-            'title': event.title,
-            'start_day': event.start_time.day,
-            'start_month': event.start_time.strftime('%b'),
-            'start_time': event.start_time,
-            'end_time': event.end_time,
-            'image': event.image.url,
-            'location': event.location
-        }
-
-        return data
+from ubyssey.zones import EventSidebar
 
 @register.widget
 class EventWidget(Widget):
