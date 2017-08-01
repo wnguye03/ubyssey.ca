b1b78bc5419f7dd7fa557922393ff8b42c6a1e80
Jonathan Chapple <joncpl@gmail.com>
2017-08-01 16:53:10 -0700

    add start and end time

--- ubyssey/widgets.py
+++ ubyssey/widgets.py
@@ -109,3 +109,20 @@ class FacebookVideoBig(Widget):
     title = CharField('Title')
     video_url = CharField('Video URL')
     show_comments = IntegerField('Show Comment Box (0 or 1)', min_value=0, max_value=1) # Should be a BoolField, ie. checkbox
+
+    start_time = DateTimeField('Start Time')
+    end_time = DateTimeField('End Time')
+
+    def context(self, result):
+        today = datetime.today()
+        do_show = True
+
+        if result['start_time'] and today < result['start_time'].replace(tzinfo=None):
+            do_show = False
+
+        if result['end_time'] and today > result['end_time'].replace(tzinfo=None):
+            do_show = False
+
+        result['do_show'] = do_show
+
+        return result
