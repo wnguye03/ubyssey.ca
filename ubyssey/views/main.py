0b1ea63212eb0607c74fb930622da932f5eea902
Peter Siemens <peterjsiemens@gmail.com>
2017-08-25 12:42:35 -0700

    Update template path

--- ubyssey/views/main.py
+++ ubyssey/views/main.py
@@ -128,7 +128,7 @@ class UbysseyTheme(DefaultTheme):
             'id': article.parent_id,
             'headline': article.headline,
             'url': article.get_absolute_url(),
-            'html': loader.render_to_string(article.get_template(), context)
+            'html': loader.render_to_string(article.get_template_path(), context)
         }
 
         return HttpResponse(json.dumps(data), content_type='application/json')
