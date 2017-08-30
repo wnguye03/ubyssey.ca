ab171e0915bb21bc95bb638cfe535129a34cb0c6
Axel-Jacobsen <axelnj44@gmail.com>
2017-08-29 17:05:38 -0700

    Event RSS Refinement

--- ubyssey/events/management/commands/EventRSS.py
+++ ubyssey/events/management/commands/EventRSS.py
@@ -2,6 +2,8 @@ import re, datetime
 
 import feedparser
 
+from django.core.management.base import BaseCommand
+
 from ubyssey.events.models import Event, ScrapedEvent
 
 class RSSFeed(object):
@@ -10,33 +12,6 @@ class RSSFeed(object):
         self.url = url
         self.feed = feedparser.parse(self.url)
         self.scrape_time = datetime.datetime.now()
-
-    def get_new_events(self):
-        """Returns set of new event GUIDs"""
-
-        yesterday = datetime.date.today() - datetime.timedelta(days=1)
-        new_events = []
-
-        # Get events in the Ubyssey database
-        scraped_events = ScrapedEvent.objects.filter(scrape_time__gte=yesterday)
-        previous_events = set(event.GUID for event in scraped_events)
-
-        # Get events from the RSS Stream
-        for event in self.feed.entries:
-
-            guid = self.get_GUID(event.links[0]['href'])
-
-            if guid not in scraped_events:
-
-                event_data = {
-                    'title': event['title'],
-                    'description': event['summary'],
-                    'GUID': guid
-                }
-
-                new_events.append(event_data)
-
-        return new_events
 
     def create_new_events(self):
         """The new events must be added to both the Event Model and ScrapedEvent"""
@@ -61,7 +36,6 @@ class RSSFeed(object):
                 end_time=self.scrape_time + datetime.timedelta(days=7),
                 location='CHANGE FIELD',
                 category='other',
-                is_submission=True,
                 submitter_email='noreply@ubyssey.ca'
                 )
             event_for_approval.save()
@@ -71,13 +45,32 @@ class RSSFeed(object):
 
         scraped_events = ScrapedEvent.objects.all()
 
-        new_events = self.get_new_events()
-        new_event_GUIDs = [event.get('GUID') for event in new_events]
+        raw_events = self.get_event_data()
+        raw_event_GUIDs = [event.get('GUID') for event in raw_events]
 
-        for event in scraped_event:
+        for event in scraped_events:
 
-            if event.GUID not in new_event_GUIDs:
+            if event.GUID not in raw_event_GUIDs:
                 event.delete()
+
+    def get_new_events(self):
+        """Returns set of new event GUIDs"""
+
+        yesterday = datetime.date.today() - datetime.timedelta(days=1)
+        new_events = []
+
+        # Get events in the Ubyssey database
+        scraped_events = ScrapedEvent.objects.filter(scrape_time__gte=yesterday)
+        previous_event_GUIDs = set(event.GUID for event in scraped_events)
+
+        # Get events from the RSS Stream
+        for event in self.get_event_data():
+
+            if event['GUID'] not in previous_event_GUIDs:
+
+                new_events.append(event)
+
+        return new_events
 
     def get_GUID(self, url):
         """returns GUID from url"""
@@ -89,5 +82,32 @@ class RSSFeed(object):
         else:
             raise FeedError('Error parsing GUID from event link')
 
+    def get_event_data(self):
+        """Gets all events from feed"""
+        events = []
+
+        for event in self.feed.entries:
+
+            guid = self.get_GUID(event.links[0]['href'])
+
+            event_data = {
+                'title': event['title'],
+                'description': event['summary'],
+                'GUID': guid
+            }
+
+            events.append(event_data)
+
+        return events
+
 class FeedError(Exception):
     pass
+
+class Command(BaseCommand):
+
+    def handle(self, **options):
+
+        feedObj = RSSFeed('http://services.calendar.events.ubc.ca/cgi-bin/rssCache.pl?days=2&mode=rss')
+
+        feedObj.create_new_events()
+        feedObj.remove_old_events()
