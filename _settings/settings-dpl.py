4f57d8a5703248b2d580640923e42676b9074afd
Peter Siemens <peterjsiemens@gmail.com>
2017-09-01 22:22:25 -0700

    Fix deploy directory

--- _settings/settings-dpl.py
+++ _settings/settings-dpl.py
@@ -16,6 +16,6 @@ STATICFILES_DIRS += (
     os.path.join(os.path.dirname(__file__), 'static/dist'),
 )
 
-STATIC_ROOT = '/home/travis/build/ubyssey/ubyssey-dispatch-theme/static/'
+STATIC_ROOT = '/home/travis/build/ubyssey/ubyssey-dispatch-theme/gcs/static'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
