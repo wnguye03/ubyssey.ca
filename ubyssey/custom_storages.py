43e2771f65e84c806e8bafac9587728d9948fa49
Peter Siemens <peterjsiemens@gmail.com>
2017-09-03 11:00:39 -0700

    Use django-google-storage

--- ubyssey/custom_storages.py
+++ ubyssey/custom_storages.py
@@ -1,8 +1,9 @@
 from django.conf import settings
-from storages.backends.gs import GSBotoStorage
+#from storages.backends.gs import GSBotoStorage
+from django_google_storage.storage import GoogleStorage
 
-class StaticStorage(GSBotoStorage):
+class StaticStorage(GoogleStorage):
     location = settings.STATICFILES_LOCATION
 
-class MediaStorage(GSBotoStorage):
+class MediaStorage(GoogleStorage):
     location = settings.MEDIAFILES_LOCATION
