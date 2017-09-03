f1f87e8f288009d0239d8be5b947c194de933890
Peter Siemens <peterjsiemens@gmail.com>
2017-09-03 09:37:58 -0700

    Remove tempfile patch

--- appengine_config.py
+++ appengine_config.py
@@ -1,8 +1,8 @@
 from google.appengine.ext import vendor
 
-# Patch to fix GCS in django-storages
-import tempfile
-tempfile.SpooledTemporaryFile = tempfile.TemporaryFile
+# # Patch to fix GCS in django-storages
+# import tempfile
+# tempfile.SpooledTemporaryFile = tempfile.TemporaryFile
 
 # Add any libraries installed in the "lib" folder.
 vendor.add('lib')
