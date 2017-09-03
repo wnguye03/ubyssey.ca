63999c0a22e14a9f6cc4f3e0c55409d8a466ee86
Peter Siemens <peterjsiemens@gmail.com>
2017-09-03 10:47:55 -0700

    Retry patch

--- appengine_config.py
+++ appengine_config.py
@@ -3,11 +3,11 @@ from google.appengine.ext import vendor
 import tempfile
 
 # Patch to fix GCS in django-storages
-class SpooledTemporaryFile(tempfile.TemporaryFile):
+class CustomSpooledTemporaryFile(tempfile.TemporaryFile):
     def __init__(max_size=0, mode='w+b', bufsize=-1, suffix='', prefix='tmp', dir=None):
         super(SpooledTemporaryFile, self).__init__(mode, bufsize, suffix, prefix, dir)
 
-tempfile.SpooledTemporaryFile = SpooledTemporaryFile
+tempfile.SpooledTemporaryFile = CustomSpooledTemporaryFile
 
 # Add any libraries installed in the "lib" folder.
 vendor.add('lib')
