bba4acc690d4e3076a8faa4ca93fd8556a50ddeb
JamieRL <lee.jamr@gmail.com>
2018-06-15 10:47:18 -0700

    removing password from settings local file

--- _settings/settings-local.py
+++ _settings/settings-local.py
@@ -31,7 +31,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ubyssey',
         'USER': 'root',
-        'PASSWORD': 'ubyssey',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     },
