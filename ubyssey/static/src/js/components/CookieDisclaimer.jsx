8d9aa63db440a00280cc4c46b6aac1590e52da87
rowansdabomb <rowanskye@gmail.com>
2018-06-22 13:01:36 -0700

    update style add link to cookie policy

--- ubyssey/static/src/js/components/CookieDisclaimer.jsx
+++ ubyssey/static/src/js/components/CookieDisclaimer.jsx
@@ -60,7 +60,7 @@ class CookieDisclaimer extends Component {
                 <p>
                   This website uses cookies to give you the best experience possible. 
                   Using this website means you accept our use of cookies. You can disable cookies in your broswer settings.
-                  We respect your privacy and you can read more about our cookie policy <em><a href={this.props.url}>here</a></em>.
+                  We respect your privacy and you can read more about our cookie policy <em><a href={'https://www.ubyssey.ca/cookie-policy-2018/'}>here</a></em>.
                 </p>
                 <button 
                   className='c-button c-button--small'
