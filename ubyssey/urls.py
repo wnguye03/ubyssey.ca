7777e704d82f5248199c4a0464b6b1859746118c
JamieRL <lee.jamr@gmail.com>
2018-07-04 10:35:11 -0700

    test view setup

--- ubyssey/urls.py
+++ ubyssey/urls.py
@@ -58,11 +58,11 @@ urlpatterns = [
     # Events
     url(r'^events/', include(events_urls)),
     url(r'^api/events/', include(event_api_urls)),
+    url(r'^test/$', theme.cron_test, name='cron-test'),
 
     url(r'^(?P<section>[-\w]+)/(?P<slug>[-\w]+)/$', theme.article, name='article'),
     url(r'^(?P<slug>[-\w]+)/$', theme.section, name='page'),
     url(r'^api/articles/(?P<pk>[0-9]+)/rendered/$', theme.article_ajax, name='article-ajax'),
-#    url(r'^api/test/$', theme.cron_test, name='cron-test')
 ]
 
 if settings.DEBUG:
