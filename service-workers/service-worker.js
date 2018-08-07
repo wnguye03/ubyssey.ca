5ee2ff68f132c4caf94c3c3a91e51bf6c1aeaec4
rowansdabomb <rowanskye@gmail.com>
2018-08-07 11:06:21 -0700

    make service worker static

--- service-workers/service-worker.js
+++ service-workers/service-worker.js
@@ -1,10 +1,11 @@
 self.addEventListener('push', function (event) {
   const data = JSON.parse(event.data.text())
   const title = data.headline;
+  console.log(data)
   const options = {
     body: data.snippet,
     data: data.url,
-    icon: '/static/images/ubyssey-logo-square.png',
+    icon: data.tag === 'breaking' ? '/static/images/ubyssey-logo-square.png' : '/static/images/breaking-news-square.png',
     badge: 'static/images/ubyssey_icon.png',
     tag: data.tag,
     image: data.image
