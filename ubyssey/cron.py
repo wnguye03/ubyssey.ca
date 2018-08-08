c5af4845421d30ea5c8fd7500a14e910eff0044d
JamieRL <lee.jamr@gmail.com>
2018-08-08 16:07:49 -0700

    remove cron.py file until we re add push notifications

--- ubyssey/cron.py
+++ ubyssey/cron.py
@@ -1,19 +1,19 @@
-import kronos
-import requests
-
-from django.conf import settings
-from django.core.urlresolvers import reverse
-
-# Setup cron jobs
-
-# Cron task for pushing notifications, if there are any, every minute
-@kronos.register('* * * * *')
-def send_notifications():
-    url = "%s%s" % (settings.BASE_URL.strip('/'), reverse('api-notifications-push'))
-    requests.post(url)
-
-# Cron task for counting subscribers daily at midnight
-@kronos.register('0 0 * * *')
-def count_subscribers():
-    url = "%s%s" % (settings.BASE_URL.strip('/'), reverse('api-subscriptioncount-list'))
-    requests.post(url)
+# import kronos
+# import requests
+#
+# from django.conf import settings
+# from django.core.urlresolvers import reverse
+#
+# # Setup cron jobs
+#
+# # Cron task for pushing notifications, if there are any, every minute
+# @kronos.register('* * * * *')
+# def send_notifications():
+#     url = "%s%s" % (settings.BASE_URL.strip('/'), reverse('api-notifications-push'))
+#     requests.post(url)
+#
+# # Cron task for counting subscribers daily at midnight
+# @kronos.register('0 0 * * *')
+# def count_subscribers():
+#     url = "%s%s" % (settings.BASE_URL.strip('/'), reverse('api-subscriptioncount-list'))
+#     requests.post(url)
