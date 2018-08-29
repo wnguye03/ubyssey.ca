c4eb477b947531eed13209d647c55c7638adf293
JamieRL <lee.jamr@gmail.com>
2018-08-29 13:12:14 -0700

    Version 1.4.53

--- _settings/settings-prd.py
+++ _settings/settings-prd.py
@@ -10,7 +10,7 @@ CANONICAL_DOMAIN = 'www.ubyssey.ca'
 SECRET_KEY = Secrets.get('SECRET_KEY')
 NOTIFICATION_KEY = Secrets.get('NOTIFICATION_KEY')
 
-VERSION = '1.4.52'
+VERSION = '1.4.53'
 
 ALLOWED_HOSTS = [
     'ubyssey.ca',
