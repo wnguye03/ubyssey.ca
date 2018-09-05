97432f3cb90a8220c83fdde4e810584147d37cd6
JamieRL <lee.jamr@gmail.com>
2018-09-05 15:28:01 -0700

    Add seperator bar between subsections in header, added helper for getting subsections, choose featured subsection based on which subsection has the most recently published article

--- ubyssey/helpers.py
+++ ubyssey/helpers.py
@@ -6,7 +6,7 @@ from django.http import Http404
 from django.db import connection
 from django.db.models.aggregates import Count
 
-from dispatch.models import Article, Page, Section
+from dispatch.models import Article, Page, Section, Subsection
 
 from ubyssey.events.models import Event
 
@@ -306,3 +306,22 @@ class PageHelper(object):
         """
 
         return Page.objects.get(request=request, slug=slug, is_published=True)
+
+class SubsectionHelper(object):
+
+    @staticmethod
+    def get_subsections(section):
+        context = {
+            'section_id': section.id
+        }
+
+        query = """
+            SELECT * FROM dispatch_subsection
+            INNER JOIN dispatch_article on dispatch_article.subsection_id = dispatch_subsection.id
+            WHERE dispatch_subsection.is_active = 1
+            AND dispatch_subsection.section_id = %(section_id)s
+            AND dispatch_article.is_published = 1
+            ORDER BY dispatch_article.published_at DESC
+        """
+
+        return list(Subsection.objects.raw(query, context))
--- ubyssey/views/main.py
+++ ubyssey/views/main.py
@@ -16,7 +16,7 @@ from dispatch.models import Article, Section, Subsection, Topic, Person
 
 import ubyssey
 import ubyssey.cron
-from ubyssey.helpers import ArticleHelper, PageHelper
+from ubyssey.helpers import ArticleHelper, PageHelper, SubsectionHelper
 
 def parse_int_or_none(maybe_int):
     try:
@@ -241,15 +241,12 @@ class UbysseyTheme(object):
 
         query = request.GET.get('q', False)
 
-        subsections = Subsection.objects.filter(section=section, is_active=True)
-        featured_subsections = []
-        for subsection in subsections:
-            most_recent_publish_date = subsection.get_published_articles().first().published_at
-            featured_subsections.append((subsection, most_recent_publish_date))
-        print('suup',featured_subsections)
+        subsections = SubsectionHelper.get_subsections(section)#Subsection.objects.filter(section=section, is_active=True)
 
-        featured_subsections = sorted(featured_subsections, key=lambda tup: tup[1])
-        print('suup',featured_subsections)
+        featured_subsection = None
+
+        if subsections:
+            featured_subsection = subsections[0]
 
         featured_articles = Article.objects.filter(section=section, is_published=True).exclude(subsection__in=subsections).order_by('-published_at')
 
@@ -277,6 +274,7 @@ class UbysseyTheme(object):
             },
             'section': section,
             'subsections': subsections,
+            'featured_subsection': featured_subsection,
             'type': 'section',
             'featured_articles': {
                 'first': featured_articles[0],
