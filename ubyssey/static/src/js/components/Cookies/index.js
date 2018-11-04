9f507a7d43f5b0201d0725111585d7fde12acac3
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2018-11-03 22:37:52 -0700

    Simplify cookie disclaimer logic

---
+++ ubyssey/static/src/js/components/Cookies/index.js
@@ -0,0 +1,7 @@
+import AdblockSplash from './AdblockSplash.jsx'
+import CookieDisclaimer from './CookieDisclaimer.jsx'
+
+export {
+  AdblockSplash as AdblockSplash,
+  CookieDisclaimer as CookieDisclaimer
+}
