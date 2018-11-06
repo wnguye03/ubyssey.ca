8635f90001f6a421de79f4ca98003f1ad1c2c673
rowansdabomb <rowanskye@gmail.com>
2018-11-06 12:59:08 -0800

    update ubyssey to version 1.4.78

--- _settings/settings-prd.py
+++ _settings/settings-prd.py
@@ -10,7 +10,7 @@ CANONICAL_DOMAIN = 'www.ubyssey.ca'
 SECRET_KEY = Secrets.get('SECRET_KEY')
 NOTIFICATION_KEY = Secrets.get('NOTIFICATION_KEY')
 
-VERSION = '1.4.77' 
+VERSION = '1.4.78' 
 
 ALLOWED_HOSTS = [
     'ubyssey.ca',
