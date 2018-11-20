6f35e7368a55b0bda1aa820accdb376a5cba4a86
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2018-11-19 17:41:54 -0800

    Update manifest + update service-worker

--- service-workers/service-worker.js
+++ service-workers/service-worker.js
@@ -1,3 +1,56 @@
+var VERSION = '1.4.80'
+var CACHE_NAME = 'ubyssey-cache-v' + VERSION;
+var urlsToCache = [
+  '/static/js/vendors-' + VERSION + '.js',
+  '/static/js/main-' + VERSION + '.js',
+  '/static/js/article-' + VERSION + '.js',
+  '/static/images/ubyssey-logo.svg'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Clear old cache
+self.addEventListener('activate', function(event) {
+
+  var cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+
 self.addEventListener('push', function (event) {
   const data = JSON.parse(event.data.text())
   const title = data.headline;
