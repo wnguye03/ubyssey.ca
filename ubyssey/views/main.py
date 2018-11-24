4c086c433b8ebd3af16e05cf623cca680be72324
rowansdabomb <rowanskye@gmail.com>
2018-11-23 17:37:20 -0800

    fix no map render

--- ubyssey/views/main.py
+++ ubyssey/views/main.py
@@ -154,7 +154,8 @@ class UbysseyTheme(object):
         if article.template == 'food-insecurity':
             data = FoodInsecurityHelper.prepare_data(article.content)
             article.content = data['content']
-            article.point_data = json.dumps(data['code'])
+            article.point_data = json.dumps(data['code']) if data['code'] is not None else None
+            print(article.point_data)
        
         ref = request.GET.get('ref', None)
         dur = request.GET.get('dur', None)
