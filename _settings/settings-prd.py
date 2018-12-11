0029c676194799a0d174ceced60514bcd2fa7cd4
rowansdabomb <rowanskye@gmail.com>
2018-12-10 21:26:31 -0800

    update ubyssey.ca to 1.4.88

--- _settings/settings-prd.py
+++ _settings/settings-prd.py
@@ -10,7 +10,7 @@ CANONICAL_DOMAIN = 'www.ubyssey.ca'
 SECRET_KEY = Secrets.get('SECRET_KEY')
 NOTIFICATION_KEY = Secrets.get('NOTIFICATION_KEY')
 
-VERSION = '1.4.87' 
+VERSION = '1.4.88' 
 
 ALLOWED_HOSTS = [
     'ubyssey.ca',
--- service-workers/service-worker.js
+++ service-workers/service-worker.js
@@ -1,4 +1,4 @@
-var VERSION = '1.4.87'
+var VERSION = '1.4.88'
 var CACHE_NAME = 'ubyssey-cache-v' + VERSION;
 var urlsToCache = [
   // js files
