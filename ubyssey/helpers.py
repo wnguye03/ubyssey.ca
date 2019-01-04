6b248cd3331bbbd2ccb0d26429ece31b7c7513bb
rowansdabomb <rowanskye@gmail.com>
2019-01-03 18:19:33 -0800

    fix reference before assignment error

--- ubyssey/helpers.py
+++ ubyssey/helpers.py
@@ -158,6 +158,7 @@ class ArticleHelper(object):
 
     @staticmethod
     def get_reading_list(article, ref=None, dur=None):
+        articles = None
         if ref is not None:
             if ref == 'frontpage':
                 articles = ArticleHelper.get_frontpage(exclude=[article.parent_id])
