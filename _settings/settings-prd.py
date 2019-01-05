1552bd77c52ed0ced57d6329e2b88acea49c67f3
rowansdabomb <rowanskye@gmail.com>
2019-01-04 16:37:27 -0800

    update to version 1.4.91

--- _settings/settings-prd.py
+++ _settings/settings-prd.py
@@ -10,7 +10,7 @@ CANONICAL_DOMAIN = 'www.ubyssey.ca'
 SECRET_KEY = Secrets.get('SECRET_KEY')
 NOTIFICATION_KEY = Secrets.get('NOTIFICATION_KEY')
 
-VERSION = '1.4.90' 
+VERSION = '1.4.91' 
 
 ALLOWED_HOSTS = [
     'ubyssey.ca',
--- service-workers/service-worker.js
+++ service-workers/service-worker.js
@@ -1,4 +1,4 @@
-var VERSION = '1.4.90'
+var VERSION = '1.4.91'
 var CACHE_NAME = 'ubyssey-cache-v' + VERSION;
 var urlsToCache = [
   // js files
