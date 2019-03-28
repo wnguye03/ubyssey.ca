4e82e94d16c1dff39381694ffe337e7630aaabd7
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2019-03-28 13:28:04 -0700

    Gulp temp UNDO LATER

--- ubyssey/static/gulpfile.js
+++ ubyssey/static/gulpfile.js
@@ -16,75 +16,10 @@ var sourcemaps = require('gulp-sourcemaps');
 var fs = require('fs');
 var version = JSON.parse(fs.readFileSync('./package.json')).version;
 
-gulp.task('webpack:build', ['clean:js'], function(callback) {
-  webpack(webpackProdConfig, function(err, stats) {
-    if (err) {
-      throw new gutil.PluginError('webpack:build', err);
-    }
-
-    gutil.log('[webpack:build]', stats.toString({ colors: true }));
-
-    callback();
-  });
-});
-
-gulp.task('webpack:build-dev', ['clean:js'], function(callback) {
-  webpack(webpackDevConfig, function(err, stats) {
-    if (err) {
-      throw new gutil.PluginError('webpack:build-dev', err);
-    }
-
-    gutil.log('[webpack:build-dev]', stats.toString({ colors: true }));
-
-    callback();
-  });
-});
-
-gulp.task('jasmine', function() {
-  gulp.src('./src/**/*.spec.js')
-    .pipe(jasmine({verbose: true}));
-});
-
-function renameFunc(path) {
-  path.basename += '-' + version;
-}
-
-gulp.task('sass:build', ['clean:css'], function() {
-  return gulp.src('./src/styles/**/*.scss')
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(rename(renameFunc))
-    .pipe(gulp.dest('./dist/css/'));
-});
-
-gulp.task('sass:build-dev', ['clean:css'], function() {
-  return gulp.src('./src/styles/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(rename(renameFunc))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dist/css/'));
-});
-
-gulp.task('copy:images', ['clean:images'], function() {
-  return gulp.src('./src/images/**/*')
-    .pipe(gulp.dest('./dist/images/'));
-});
-
-gulp.task('copy:videos', ['clean:videos'], function() {
-  return gulp.src('./src/videos/**/*')
-    .pipe(gulp.dest('./dist/videos/'));
-});
-
-gulp.task('copy:fonts', ['clean:fonts'], function() {
-  return gulp.src('./src/fonts/**/*')
-    .pipe(gulp.dest('./dist/fonts/'));
-});
-
 gulp.task('clean:js', function() {
   return gulp.src('./dist/js/', {read: false})
     .pipe(clean());
 });
-
 gulp.task('clean:css', function() {
   return gulp.src('./dist/css/', {read: false})
     .pipe(clean());
@@ -105,14 +40,84 @@ gulp.task('clean:fonts', function() {
     .pipe(clean());
 });
 
-gulp.task('build', ['webpack:build', 'sass:build', 'copy:images', 'copy:videos', 'copy:fonts']);
+gulp.task('webpack:build', function() {
+ gulp.series('clean:js', function(callback) {
+   webpack(webpackProdConfig, function(err, stats) {
+     if (err) {
+       throw new gutil.PluginError('webpack:build', err);
+     }
 
-gulp.task('build-dev', ['webpack:build-dev', 'sass:build-dev', 'copy:images', 'copy:videos', 'copy:fonts']);
+     gutil.log('[webpack:build]', stats.toString({ colors: true }));
 
-gulp.task('default', ['build-dev'], function() {
-  gulp.watch(['./src/js/**/*'],     ['webpack:build-dev']);
-  gulp.watch(['./src/styles/**/*'], ['sass:build-dev']);
-  gulp.watch(['./src/images/**/*'], ['copy:images']);
-  gulp.watch(['./src/videos/**/*'], ['copy:videos']);
-  gulp.watch(['./src/fonts/**/*'],  ['copy:fonts']);
+     callback();
+   });
+ });
 });
+
+gulp.task('webpack:build-dev', function() {
+  gulp.series('clean:js', function(callback) {
+    webpack(webpackDevConfig, function(err, stats) {
+      if (err) {
+        throw new gutil.PluginError('webpack:build-dev', err);
+      }
+
+      gutil.log('[webpack:build-dev]', stats.toString({ colors: true }));
+
+      callback();
+    });
+  })
+});
+
+gulp.task('jasmine', function() {
+  gulp.src('./src/**/*.spec.js')
+    .pipe(jasmine({verbose: true}));
+});
+
+function renameFunc(path) {
+  path.basename += '-' + version;
+}
+
+gulp.task('sass:build', function() {
+  gulp.series('clean:css', function() {
+    return gulp.src('./src/styles/**/*.scss')
+      .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+      .pipe(rename(renameFunc))
+      .pipe(gulp.dest('./dist/css/'));
+  });
+});
+
+gulp.task('sass:build-dev', gulp.series('clean:css', function() {
+  return gulp.src('./src/styles/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(rename(renameFunc))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/css/'));
+}));
+
+gulp.task('copy:images', gulp.series('clean:images', function() {
+  return gulp.src('./src/images/**/*')
+    .pipe(gulp.dest('./dist/images/'));
+}));
+
+gulp.task('copy:videos', gulp.series('clean:videos', function() {
+  return gulp.src('./src/videos/**/*')
+    .pipe(gulp.dest('./dist/videos/'));
+}));
+
+gulp.task('copy:fonts', gulp.series('clean:fonts', function() {
+  return gulp.src('./src/fonts/**/*')
+    .pipe(gulp.dest('./dist/fonts/'));
+}));
+
+gulp.task('build', gulp.series('webpack:build', 'sass:build', 'copy:images', 'copy:videos', 'copy:fonts'));
+
+gulp.task('build-dev', gulp.series('webpack:build-dev', 'sass:build-dev', 'copy:images', 'copy:videos', 'copy:fonts'));
+
+gulp.task('default', gulp.series('build-dev', function() {
+  gulp.watch('./src/js/**/*',     'webpack:build-dev');
+  gulp.watch('./src/styles/**/*', 'sass:build-dev');
+  gulp.watch('./src/images/**/*', 'copy:images');
+  gulp.watch('./src/videos/**/*', 'copy:videos');
+  gulp.watch('./src/fonts/**/*',  'copy:fonts');
+}));
