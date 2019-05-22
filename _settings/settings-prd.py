48dcb5a058bedb52b0a5e580f0319767688d0e3c
Razvan Nesiu <razvannesiu@gmail.com>
2019-05-22 15:32:47 -0700

    Update ubyssey to version 1.5.2

--- _settings/settings-prd.py
+++ _settings/settings-prd.py
@@ -10,7 +10,7 @@ CANONICAL_DOMAIN = 'www.ubyssey.ca'
 SECRET_KEY = Secrets.get('SECRET_KEY')
 NOTIFICATION_KEY = Secrets.get('NOTIFICATION_KEY')
 
-VERSION = '1.5.1'
+VERSION = '1.5.2'
 
 ALLOWED_HOSTS = [
     'ubyssey.ca',
--- service-workers/service-worker.js
+++ service-workers/service-worker.js
@@ -1,4 +1,4 @@
-var VERSION ='1.5.1'
+var VERSION ='1.5.2'
 var CACHE_NAME = 'ubyssey-cache-v' + VERSION;
 var urlsToCache = [
   // js files
