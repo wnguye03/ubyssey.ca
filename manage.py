f85a86438f228045ab8628a49e666b914110ebda
Amelia He <amelia.he98@gmail.com>
2019-05-23 21:43:13 -0700

    tried to format this so it has an entry point

--- manage.py
+++ manage.py
@@ -2,7 +2,9 @@
 import os
 import sys
 
-if __name__ == "__main__":
+app = __name__
+
+if app == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ubyssey.settings")
 
     from django.core.management import execute_from_command_line
