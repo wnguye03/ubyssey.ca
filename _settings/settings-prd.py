98141cd25b86307b3b25af03ee374d643d236ea6
Razvan Nesiu <razvannesiu@gmail.com>
2019-05-27 17:50:25 -0700

    Prime commit # 2237: Update ubyssey to version 1.5.38 (env: flex)

--- _settings/settings-prd.py
+++ _settings/settings-prd.py
@@ -16,7 +16,7 @@ def getValue(theKey):
 SECRET_KEY = getValue('SECRET_KEY')
 NOTIFICATION_KEY = getValue('NOTIFICATION_KEY')
 
-VERSION = '1.5.37'
+VERSION = '1.5.38'
 
 ALLOWED_HOSTS = [
     'ubyssey.ca',
--- service-workers/service-worker.js
+++ service-workers/service-worker.js
@@ -1,4 +1,4 @@
-var VERSION ='1.5.37'
+var VERSION ='1.5.38'
 var CACHE_NAME = 'ubyssey-cache-v' + VERSION;
 var urlsToCache = [
   // js files
