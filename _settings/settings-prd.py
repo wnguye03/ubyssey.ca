4f99fd14584dd4248b87b2b8b4d715c7f40fc7fb
Razvan Nesiu <razvannesiu@gmail.com>
2019-05-31 12:28:20 -0700

    Update ubyssey to v1.5.53 (froze django-google-storage to v0.5.4)

--- _settings/settings-prd.py
+++ _settings/settings-prd.py
@@ -16,7 +16,7 @@ def getValue(theKey):
 SECRET_KEY = getValue('SECRET_KEY')
 NOTIFICATION_KEY = getValue('NOTIFICATION_KEY')
 
-VERSION = '1.5.52'
+VERSION = '1.5.53'
 
 ALLOWED_HOSTS = [
     'ubyssey.ca',
--- service-workers/service-worker.js
+++ service-workers/service-worker.js
@@ -1,4 +1,4 @@
-var VERSION ='1.5.52'
+var VERSION ='1.5.53'
 var CACHE_NAME = 'ubyssey-cache-v' + VERSION;
 var urlsToCache = [
   // js files
