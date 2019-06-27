008b44fdff06c53fc51d88ee4de271e7e2a7e97f
Razvan Nesiu <razvannesiu@gmail.com>
2019-06-26 18:15:17 -0700

    Update ubyssey to v1.6.3 (add video on frontpage)

--- _settings/settings-prd.py
+++ _settings/settings-prd.py
@@ -16,7 +16,7 @@ def getValue(theKey):
 SECRET_KEY = getValue('SECRET_KEY')
 NOTIFICATION_KEY = getValue('NOTIFICATION_KEY')
 
-VERSION = '1.6.2'
+VERSION = '1.6.3'
 
 ALLOWED_HOSTS = [
     'ubyssey.ca',
--- service-workers/service-worker.js
+++ service-workers/service-worker.js
@@ -1,4 +1,4 @@
-var VERSION ='1.6.2'
+var VERSION ='1.6.3'
 var CACHE_NAME = 'ubyssey-cache-v' + VERSION;
 var urlsToCache = [
   // js files
