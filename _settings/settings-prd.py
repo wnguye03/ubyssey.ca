afa56771c6c47b21b21c333de9be46703e7e06f3
Amelia He <amelia.he98@gmail.com>
2019-07-05 23:48:29 -0700

    new version release

--- _settings/settings-prd.py
+++ _settings/settings-prd.py
@@ -16,7 +16,7 @@ def getValue(theKey):
 SECRET_KEY = getValue('SECRET_KEY')
 NOTIFICATION_KEY = getValue('NOTIFICATION_KEY')
 
-VERSION = '1.6.4'
+VERSION = '1.6.5'
 
 ALLOWED_HOSTS = [
     'ubyssey.ca',
