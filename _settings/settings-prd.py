8039bdb50136683a62f00790a4bb6f42296c67d3
davidkao0902 <davidkao0902@hotmail.com>
2020-05-27 17:13:32 -0700

    fix ad id issue

--- _settings/settings-prd.py
+++ _settings/settings-prd.py
@@ -16,7 +16,7 @@ def getValue(theKey):
 SECRET_KEY = getValue('SECRET_KEY')
 NOTIFICATION_KEY = getValue('NOTIFICATION_KEY')
 
-VERSION = '1.6.30'
+VERSION = '1.6.31'
 
 ALLOWED_HOSTS = [
     'ubyssey.ca',
