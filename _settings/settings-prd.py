98b6392ed1606cce1b42c4fcafeea5c887c62750
davidkao0902 <davidkao0902@hotmail.com>
2020-05-28 12:00:47 -0700

    Ad-unique

--- _settings/settings-prd.py
+++ _settings/settings-prd.py
@@ -16,7 +16,7 @@ def getValue(theKey):
 SECRET_KEY = getValue('SECRET_KEY')
 NOTIFICATION_KEY = getValue('NOTIFICATION_KEY')
 
-VERSION = '1.6.31'
+VERSION = '1.6.32'
 
 ALLOWED_HOSTS = [
     'ubyssey.ca',
--- service-workers/service-worker.js
+++ service-workers/service-worker.js
@@ -1,4 +1,4 @@
-var VERSION ='1.6.31'
+var VERSION ='1.6.32'
 var CACHE_NAME = 'ubyssey-cache-v' + VERSION;
 var urlsToCache = [
   // js files
