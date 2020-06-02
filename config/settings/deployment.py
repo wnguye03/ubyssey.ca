77082eac9c3170d519992c22491b35a8db3b00c9
Keegan Landrigan <keeganland@gmail.com>
2020-06-02 10:18:35 -0700

    Init version of settings module. Took base settings from Dispatch to be more explicit - but may need to restore the stronger dependency in Dispatch's end

---
+++ config/settings/deployment.py
@@ -0,0 +1,19 @@
+# Deployment settings
+
+import os
+
+from .base import *
+
+SECRET_KEY = 'TEMP-KEY'
+
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS += ['ubyssey.events',]
+
+STATICFILES_DIRS += (
+    os.path.join(os.path.dirname(__file__), 'static/dist'),
+)
+
+STATIC_ROOT = '/home/travis/build/ubyssey/ubyssey.ca/gcs/static'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
