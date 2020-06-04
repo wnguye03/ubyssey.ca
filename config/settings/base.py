9955cb44ff2ad5e79f1e08186e52f0db4df74b26
Keegan Landrigan <keeganland@gmail.com>
2020-06-04 16:35:16 +0000

    update version variables to 1.7.1

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,7 +13,7 @@ PROJECT_DIR = environ.Path(__file__) - 3 # i.e. the /ubyssey.ca directory
 DISPATCH_APP_DIR = DispatchConfig.path
 
 ORGANIZATION_NAME = 'Ubyssey'
-VERSION = '1.7.0'
+VERSION = '1.7.1'
 
 # Application definition
 INSTALLED_APPS = [
