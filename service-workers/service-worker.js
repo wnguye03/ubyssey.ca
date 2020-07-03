6001c0dc1b949516fdb80e2bfd5ccc2a1d0e7453
davidkao0902 <davidkao0902@hotmail.com>
2020-07-03 23:09:49 +0000

    version change to 1.9.14

--- service-workers/service-worker.js
+++ service-workers/service-worker.js
@@ -1,7 +1,7 @@
 // var file = new File("../version.txt");
 // var reader = new FileReader(file);
 // var VERSION = reader.readAsText(file);
-var VERSION ='1.9.13'
+var VERSION ='1.9.14'
 var CACHE_NAME = 'ubyssey-cache-v' + VERSION;
 var urlsToCache = [
   // js files
