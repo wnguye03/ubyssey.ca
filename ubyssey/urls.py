0eed40fea4f2f7817bb4d84bba2425d83c0b8c6e
Keegan Landrigan <keeganland@gmail.com>
2021-02-21 04:16:09 +0000

    temporary new URLs

--- ubyssey/urls.py
+++ ubyssey/urls.py
@@ -11,7 +11,7 @@ from ubyssey.views.main import ads_txt, UbysseyTheme, HomePageView, ArticleView,
 from ubyssey.views.guide import guide2016, GuideArticleView, GuideLandingView
 
 from ubyssey.views.advertise import AdvertiseTheme
-from ubyssey.views.magazine import magazine
+from ubyssey.views.magazine import magazine, MagazineLandingView, MagazineArticleView
 
 from ubyssey.zones import *
 from ubyssey.widgets import *
@@ -80,6 +80,10 @@ urlpatterns += [
     # TODO: REFACTOR THESE VIEWS, figure out where there are broken URLs
     re_path(r'^magazine/(?P<year>[0-9]{4})/$', magazine.magazine, name='magazine-landing'),
     re_path(r'^magazine/(?P<slug>[-\w]+)/$', magazine.article, name='magazine-article'),
+
+    # Magazine new
+    re_path(r'^newmagazine/(?P<year>[0-9]{4})/$', MagazineLandingView.as_view(), name='magazine-landing'),
+    re_path(r'^newmagazine/(?P<slug>[-\w]+)/$', MagazineArticleView.as_view(), name='magazine-article'),
 
     # Advertising
     re_path(r'^advertise/$', advertise.new, name='advertise-new'),
