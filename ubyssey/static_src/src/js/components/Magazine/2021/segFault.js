1c72afd41256c867bc3dc35881242393eefb520f
Samuel Lin <Samuel3218888@gmail.com>
2021-03-09 19:22:22 +0000

    Magazine header add & text font change

--- ubyssey/static_src/src/js/components/Magazine/2021/segFault.js
+++ ubyssey/static_src/src/js/components/Magazine/2021/segFault.js
@@ -8,6 +8,18 @@ export default class segFault extends Component {
                 <div className="wrapper">
 
                     <div className="grid_container fade-in">
+
+
+                        <div className="header_photo nested">
+                            <div>
+                                <img className="photo_vertical" src={'https://storage.googleapis.com/ubyssey/images/Memory_Leak_Move.gif'} />
+                            </div>
+                            <div className="overlay">
+                                <span> <h1>SEG FAULT </h1> </span>
+
+                            </div>
+                        </div>
+
                         {this.props.articles.map((article, index) => {
 
                             return (
@@ -18,7 +30,7 @@ export default class segFault extends Component {
                                         <img className="photo_vertical" src={article.featured_image} />
 
                                     </div>
-                                    <div> <h2 style={{ color: 'white' }}>{article.headline}</h2></div>
+                                    <div className="headline"> <div style={{ color: 'white' }} > {article.headline} Raging against the machine: what the evolution of UBC's protests tells us about student activism's future  </div> </div>
                                     <div className="card__overlay"> <a href={article.url}> <span> Read More </span></a> </div>
                                 </div>
                             )
@@ -29,8 +41,9 @@ export default class segFault extends Component {
 
                     </div>
 
-                </div>
 
+
+                </div>
 
             </div>
         )
