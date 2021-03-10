c7ebb199987ba684d943b5f48fa43e13922473fe
Keegan Landrigan <keeganland@gmail.com>
2021-03-10 03:10:46 +0000

    use old view for mag for now

--- ubyssey/urls.py
+++ ubyssey/urls.py
@@ -81,10 +81,10 @@ urlpatterns += [
     re_path(r'^magazine/(?P<year>[0-9]{4})/$', magazine.magazine, name='magazine-landing'),
     re_path(r'^magazine/(?P<slug>[-\w]+)/$', magazine.article, name='magazine-article'),
 
-    # Magazine new
-    re_path(r'^mag/(?P<year>[0-9]{4})/$', MagazineLandingView.as_view(), name='mag-landing'),
-    re_path(r'^mag/(?P<year>[0-9]{4})/(?P<subsection>[-\w]+)/$', MagazineLandingView.as_view(), name='mag-landing-sub'),
-    re_path(r'^mag/(?P<year>[0-9]{4})/(?P<subsection>[-\w]+)/(?P<slug>[-\w]+)/$', MagazineArticleView.as_view(), name='mag-article'),
+    # # Magazine new = on pause until wagtail happens a bit more
+    # re_path(r'^mag/(?P<year>[0-9]{4})/$', MagazineLandingView.as_view(), name='mag-landing'),
+    # re_path(r'^mag/(?P<year>[0-9]{4})/(?P<subsection>[-\w]+)/$', MagazineLandingView.as_view(), name='mag-landing-sub'),
+    # re_path(r'^mag/(?P<year>[0-9]{4})/(?P<subsection>[-\w]+)/(?P<slug>[-\w]+)/$', MagazineArticleView.as_view(), name='mag-article'),
 
     # Advertising
     re_path(r'^advertise/$', advertise.new, name='advertise-new'),
