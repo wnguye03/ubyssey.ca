fcff6662a896487d73c0108a6be5f1577eaf5f63
Keegan Landrigan <keeganland@gmail.com>
2021-04-07 23:31:38 -0700

    init some comments for future design

--- home/models.py
+++ home/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
 
 # Create your models here.
 
 class HomePage(Page):
     template = "home/home_page.html"
+
+    # content = StreamField(
+    #     [
+
+    #     ]
+    # )
