bc2245bddb4280a395f87bfc5f7a86ff3e5a837b
Keegan Landrigan <keeganland@gmail.com>
2021-05-05 20:23:31 +0000

    fix GS_CREDENTIALS

--- config/settings/production.py
+++ config/settings/production.py
@@ -41,9 +41,7 @@ MEDIA_ROOT = ''
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 GS_ACCESS_KEY_ID = env('GS_ACCESS_KEY_ID')
 GS_SECRET_ACCESS_KEY = env('GS_SECRET_ACCESS_KEY')
-GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
-    os.path.join(BASE_DIR, 'ubyssey-prd-ee6290e6327f.json')
-)
+GS_CREDENTIALS = service_account.Credentials.from_service_account_file('ubyssey-prd-ee6290e6327f.json')
 GS_STORAGE_BUCKET_NAME = 'ubyssey' # See documentation https://django-storages.readthedocs.io/en/latest/backends/gcloud.html
 GS_BUCKET_NAME = GS_STORAGE_BUCKET_NAME # https://github.com/mirumee/saleor/issues/5222 see suggestion both these variables are needed
 GS_LOCATION = 'media'
