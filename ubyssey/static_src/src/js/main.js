bc36679ba8b149dfd38376fa70b4a18e580f92d9
Samuel Lin <Samuel3218888@gmail.com>
2021-05-12 18:07:50 +0000

    Update Navbar for Isolation Project

--- ubyssey/static_src/src/js/main.js
+++ ubyssey/static_src/src/js/main.js
@@ -1,15 +1,16 @@
 import * as mp from './modules/Mixpanel';
 import upcomingEvents from './widgets/upcoming-events';
 
-(function() {
+(function () {
 
   initializeSearchFormActions();
   initializeSocialMediaActions();
 
   ubysseyHeaderMobilePopUp();
   ubysseyHeaderMagazineDropDown();
+  ubysseyHeaderCultureDropDown();
   archiveMobileDropDown();
-  
+
   if ($('.js-article').length) {
     mp.pageView('article', $('.js-article'), 1)
   } else {
@@ -17,40 +18,44 @@ import upcomingEvents from './widgets/upcoming-events';
   }
 
   //listener for magazine in mobile header pop up
-  $('#magazine-mobile').click(function(){
+  $('#magazine-mobile').click(function () {
     $('#magazine-more').slideToggle(200);
+  });
+
+  $('#culture-mobile').click(function () {
+    $('#culture-more').slideToggle(200);
   });
 
   let isUpcomingEventsCreated = false;
 
-  $(document).ready(function() {
+  $(document).ready(function () {
 
     embedMargins();
 
-    if($(window).width() <= 500) {
+    if ($(window).width() <= 500) {
       resizeFullWidthStoryBanner();
     }
 
     // register widgets
-    if(!isUpcomingEventsCreated && $(window).width() >= 1200) {
+    if (!isUpcomingEventsCreated && $(window).width() >= 1200) {
       isUpcomingEventsCreated = true;
       upcomingEvents();
     }
 
     //Calls issue parser only in homepage
-    if(window.location.pathname === '/'){ issueParser(); }
-    
-  }); 
-  
-  $(window).resize(function() {
+    if (window.location.pathname === '/') { issueParser(); }
+
+  });
+
+  $(window).resize(function () {
 
     //make sure the carousel widget has correct size
-    if(!isUpcomingEventsCreated && $(window).width() >= 1200) {
+    if (!isUpcomingEventsCreated && $(window).width() >= 1200) {
       isUpcomingEventsCreated = true;
       upcomingEvents();
     }
 
-    if($(window).width() <= 500) {
+    if ($(window).width() <= 500) {
       resizeFullWidthStoryBanner();
     }
 
@@ -59,7 +64,7 @@ import upcomingEvents from './widgets/upcoming-events';
 })();
 
 function disableScroll() {
-  $(document).on('touchmove', function(e) {
+  $(document).on('touchmove', function (e) {
     e.preventDefault();
   });
   $('body').addClass('u-no-scroll');
@@ -77,13 +82,13 @@ function embedMargins() {
   $('.image-attachment.right').css('marginRight', marginRight)
 }
 
-function resizeFullWidthStoryBanner(){
-  if($('.fw-banner banner-image') !== undefined) {
+function resizeFullWidthStoryBanner() {
+  if ($('.fw-banner banner-image') !== undefined) {
     let bannerHeight = $('.banner-image').height();
     let captionHeight = $('.caption').height();
     let headlineHeight = $('.headline-container').height();
-    let minimumHeight = captionHeight+headlineHeight+30;
-    if(bannerHeight < minimumHeight) {
+    let minimumHeight = captionHeight + headlineHeight + 30;
+    if (bannerHeight < minimumHeight) {
       $('.banner-image').height(minimumHeight);
     }
   }
@@ -94,40 +99,53 @@ function issueParser() {
   var req = new XMLHttpRequest();
   req.open('GET', 'https://cors-anywhere.herokuapp.com/https://search.issuu.com/api/2_0/document?q=username%3Aubyssey&responseParams=title&sortBy=epoch&pageSize=4');
   req.setRequestHeader('Ubyssey', 'XMLHttpRequest');
-  req.onload = function() {
+  req.onload = function () {
     var res = req.responseText;
     var jsonQuery = '[' + res.split('[')[1].split(']')[0] + ']';
     var issueArray = JSON.parse(jsonQuery);
-    for(var i = 0; i < issueArray.length; i++) {
+    for (var i = 0; i < issueArray.length; i++) {
       var docName = issueArray[i].docname;
       var docId = issueArray[i].documentId;
       var title = issueArray[i].title;
-      var issueID = '#issue' + (i+1);
+      var issueID = '#issue' + (i + 1);
       $(issueID).attr('href', 'https://issuu.com/ubyssey/docs/' + docName).text(title);
-      if(i == 0)
-        $(issueID).append('<img src="https://image.isu.pub/' + docId +'/jpg/page_1_thumb_large.jpg">');
+      if (i == 0)
+        $(issueID).append('<img src="https://image.isu.pub/' + docId + '/jpg/page_1_thumb_large.jpg">');
     }
   };
   req.send();
 }
 
-function ubysseyHeaderMagazineDropDown(){
-  $('#sections-more-dropdown').hover(function(e) {
+function ubysseyHeaderMagazineDropDown() {
+  $('#sections-more-dropdown').hover(function (e) {
     e.stopPropagation();
     $('.sections-more').finish();
     $('.sections-more').slideToggle(300);
-    }, (function(e) {
-      e.stopPropagation();
-      $('.sections-more').finish();
-      $('.sections-more').fadeOut(300);
-    })
+  }, (function (e) {
+    e.stopPropagation();
+    $('.sections-more').finish();
+    $('.sections-more').fadeOut(300);
+  })
   );
 }
 
-function archiveMobileDropDown(){
+function ubysseyHeaderCultureDropDown() {
+  $('#culture-sections-more-dropdown').hover(function (e) {
+    e.stopPropagation();
+    $('.culture-sections-more').finish();
+    $('.culture-sections-more').slideToggle(300);
+  }, (function (e) {
+    e.stopPropagation();
+    $('.culture-sections-more').finish();
+    $('.culture-sections-more').fadeOut(300);
+  })
+  );
+}
+
+function archiveMobileDropDown() {
   let DROPDOWN_FADE_TIME = 100;
 
-  $('.js-dropdown-container').click(function(e) {
+  $('.js-dropdown-container').click(function (e) {
     e.preventDefault();
     var dropdown = $(this).parent();
     dropdown.fadeOut(DROPDOWN_FADE_TIME);
@@ -135,10 +153,10 @@ function archiveMobileDropDown(){
     return false;
   });
 
-  $('.js-dropdown > a').click(function(e) {
+  $('.js-dropdown > a').click(function (e) {
     e.preventDefault();
     var dropdown = $(this).parent().find('.js-dropdown-list');
-    if (dropdown.is(':visible')){
+    if (dropdown.is(':visible')) {
       dropdown.fadeOut(DROPDOWN_FADE_TIME);
       enableScroll();
     } else {
@@ -150,19 +168,19 @@ function archiveMobileDropDown(){
     return false;
   });
 
-  $('.js-dropdown-list a').click(function(e) {
+  $('.js-dropdown-list a').click(function (e) {
     e.stopPropagation();
   });
 }
 
-function ubysseyHeaderMobilePopUp(){
-  $('a.menu').click(function(e){
+function ubysseyHeaderMobilePopUp() {
+  $('a.menu').click(function (e) {
     e.preventDefault();
-    if ($('nav.mobile').is(':visible')){
+    if ($('nav.mobile').is(':visible')) {
       $('nav.mobile').hide();
       $(this).removeClass('active');
     } else {
-      if ($('#search-form').is(':visible')){
+      if ($('#search-form').is(':visible')) {
         $('#search-form').hide();
         $('a.search').removeClass('active');
       }
@@ -173,11 +191,11 @@ function ubysseyHeaderMobilePopUp(){
 }
 
 //Pending deletion
-function initializeSearch(){ 
-  $('.dropdown > a').click(function(e){
+function initializeSearch() {
+  $('.dropdown > a').click(function (e) {
     e.preventDefault();
     var dropdown = $(this).parent().find('.list');
-    if (dropdown.is(':visible')){
+    if (dropdown.is(':visible')) {
       dropdown.hide();
     } else {
       dropdown.show();
@@ -189,43 +207,43 @@ function initializeSearch(){
 
 }
 
-function initializeSearchFormActions(){
-    $(document).on('click', 'a.search', function(e){
-      e.preventDefault();
-      if ($('#search-form').is(':visible')){
-        $('#search-form').hide();
-        $(this).removeClass('active');
-      } else {
-        if ($('nav.mobile').is(':visible')){
-          $('nav.mobile').hide();
-          $('a.menu').removeClass('active');
-        }
-        $('#search-form').show();
-        $('#search-bar').focus();
-        $(this).addClass('active');
-      }
-      e.stopPropagation();
-    });
-
-    //listeners for hiding search form
-    $(document).on('keyup', function(e) {
-      var ESCAPE = 27;
-      if (e.keyCode == ESCAPE) {
-        $('#search-form').is(':visible') && $('#search-form').hide();
-      }
-    });
-  
-    $(document).click(function() {
+function initializeSearchFormActions() {
+  $(document).on('click', 'a.search', function (e) {
+    e.preventDefault();
+    if ($('#search-form').is(':visible')) {
       $('#search-form').hide();
-    });
+      $(this).removeClass('active');
+    } else {
+      if ($('nav.mobile').is(':visible')) {
+        $('nav.mobile').hide();
+        $('a.menu').removeClass('active');
+      }
+      $('#search-form').show();
+      $('#search-bar').focus();
+      $(this).addClass('active');
+    }
+    e.stopPropagation();
+  });
 
-    $(document).on('click', '#search-form > .u-container', function(e){
-      e.stopPropagation();
-    });
+  //listeners for hiding search form
+  $(document).on('keyup', function (e) {
+    var ESCAPE = 27;
+    if (e.keyCode == ESCAPE) {
+      $('#search-form').is(':visible') && $('#search-form').hide();
+    }
+  });
+
+  $(document).click(function () {
+    $('#search-form').hide();
+  });
+
+  $(document).on('click', '#search-form > .u-container', function (e) {
+    e.stopPropagation();
+  });
 }
 
-function initializeSocialMediaActions(){
-  $(document).on('click', 'a.facebook', function(e){
+function initializeSocialMediaActions() {
+  $(document).on('click', 'a.facebook', function (e) {
     e.preventDefault();
     FB.ui({
       method: 'share_open_graph',
@@ -233,18 +251,18 @@ function initializeSocialMediaActions(){
       action_properties: JSON.stringify({
         object: $(this).data('url'),
       })
-    }, function(response){});
+    }, function (response) { });
   });
 
-  $(document).on('click', 'a.twitter', function(e){
+  $(document).on('click', 'a.twitter', function (e) {
     e.preventDefault();
-    window.open('http://twitter.com/share?url=' + $(this).data('url') + '&text=' + $(this).data('title') + '&', 'twitterwindow', 
-    'height=450, width=550, top='+($(window).height()/2 - 225) +', left='+($(window).width()/2 - 225) +', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+    window.open('http://twitter.com/share?url=' + $(this).data('url') + '&text=' + $(this).data('title') + '&', 'twitterwindow',
+      'height=450, width=550, top=' + ($(window).height() / 2 - 225) + ', left=' + ($(window).width() / 2 - 225) + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
   });
 
-  $(document).on('click', 'a.reddit', function(e){
+  $(document).on('click', 'a.reddit', function (e) {
     e.preventDefault();
-    window.open('http://www.reddit.com/submit?url=' + $(this).data('url') + '&title=' + $(this).data('title') + '&', 'redditwindow', 
-    'height=450, width=550, top='+($(window).height()/2 - 225) +', left='+($(window).width()/2 - 225) +', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+    window.open('http://www.reddit.com/submit?url=' + $(this).data('url') + '&title=' + $(this).data('title') + '&', 'redditwindow',
+      'height=450, width=550, top=' + ($(window).height() / 2 - 225) + ', left=' + ($(window).width() / 2 - 225) + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
   });
 }
