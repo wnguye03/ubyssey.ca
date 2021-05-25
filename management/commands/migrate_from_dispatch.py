e0702f77e144bc82ddaddb1445a14f55052db314
Keegan Landrigan <keeganland@gmail.com>
2021-05-25 19:24:06 +0000

    some basic iteration

--- management/commands/migrate_from_dispatch.py
+++ management/commands/migrate_from_dispatch.py
@@ -1,7 +1,26 @@
+from article.models import ArticlePage
+
+from dispatch.models import Article
+
 from django.core.management.base import BaseCommand, CommandError, no_translations
+
+from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
+
 
 class Command(BaseCommand):
     
     @no_translations
     def handle(self, *args, **options):
-        pass
+
+        # dispatch_article 
+        dispatch_article_qs = Article.objects.filter(slug='keegan-test').order_by('revision_id')
+        
+        # wagtail_article
+        wagtail_article = ArticlePage()
+        for dispatch_article_revision in dispatch_article_qs(''):
+            for node in dispatch_article_revision.content:
+                pass
+                # figure out what the type of the embed is (embed_type). set the appropriate block_type
+                
+                #wagtail_article.content
