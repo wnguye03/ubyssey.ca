812f118776e25e05173928550a56f0ec4bdf993d
Keegan Landrigan <keeganland@gmail.com>
2021-07-22 19:20:28 -0700

    remove reference to lazyloading-wagtail.js

--- ubyssey/static_src/webpack.common.js
+++ ubyssey/static_src/webpack.common.js
@@ -20,7 +20,6 @@ module.exports = {
     'soccer_nationals': './src/js/soccer_nationals.js',
     'timeline': './src/js/timeline.js',
     'episode': './src/js/episode.js',
-    'lazyloading-wagtail': './src/js/lazyloading-wagtail.js',
     blockadblock: './src/js/blockadblock.js'
   },
   output: {
