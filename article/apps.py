389dda14c7a47f213b49388a95cdd29459a85cb8
Keegan Landrigan <keeganland@gmail.com>
2021-08-17 00:25:29 +0000

    signals initialization

--- article/apps.py
+++ article/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class ArticleConfig(AppConfig):
     name = 'article'
+
+    def ready(self) -> None:
+        import article.signals #see https://www.youtube.com/watch?v=Kc1Q_ayAeQk
+        return super().ready()
