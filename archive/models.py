6ef2454cd85183dca4b626d1ea761749f3fd8c1e
Keegan Landrigan <keeganland@gmail.com>
2022-07-20 23:31:15 +0000

    basic context logic for archive page

--- archive/models.py
+++ archive/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django_user_agents.utils import get_user_agent
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
-# Create your models here.
+from article.models import ArticlePage
+
+from wagtail.core.models import Page
+
+class ArchivePage(Page):
+    template = "archive/archive_page.html"
+
+    parent_page_types = [
+        'home.HomePage',
+    ]
+    max_count_per_parent = 1
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+
+        user_agent = get_user_agent(request)
+        context['is_mobile'] = user_agent.is_mobile
+
+        search_query = request.GET.get("q")
+        page = request.GET.get("page")
+        order = request.GET.get("order")
+
+        if order == 'oldest':
+            article_order = "explicit_published_at"
+        else:            
+            article_order = "-explicit_published_at"
+        context["order"] = order
+
+        # Hit the db
+        articles = ArticlePage.objects.live().public().order_by(article_order)
+        if search_query:
+            context["search_query"] = search_query
+            articles = articles.search(search_query)
+
+        # Paginate all posts by 15 per page
+        paginator = Paginator(articles, per_page=15)
+        try:
+            # If the page exists and the ?page=x is an int
+            paginated_articles = paginator.page(page)
+            context["current_page"] = page
+        except PageNotAnInteger:
+            # If the ?page=x is not an int; show the first page
+            paginated_articles = paginator.page(1)
+        except EmptyPage:
+            # If the ?page=x is out of range (too high most likely)
+            # Then return the last page
+            paginated_articles = paginator.page(paginator.num_pages)
+
+        context["paginated_articles"] = paginated_articles #this object is often called page_obj in Django docs, but Page means something else in Wagtail
+
+
+        return context
