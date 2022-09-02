56173d8c77c3a062b3ef4c5a6e747de5ad3603e6
Keegan Landrigan <keeganland@gmail.com>
2022-09-02 21:28:55 +0000

    remove signals

--- section/apps.py
+++ section/apps.py
@@ -3,7 +3,3 @@ from django.apps import AppConfig
 
 class SectionConfig(AppConfig):
     name = 'section'
-
-    def ready(self) -> None:
-        import section.sectionable.signals
-        return super().ready()
